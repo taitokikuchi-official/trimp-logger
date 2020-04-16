322c930bddf849a876ecab9221d19d1078f3a08f
taito.kikuchi <unclt2500@gmail.com>
2020-04-16 17:53:48 +0900

    jest sample

---
+++ src/TestSample.ts
@@ -0,0 +1,5 @@
+const TestSample = (a:number, b:number) => {
+    return a + b;
+}
+  
+export default TestSample;
---
+++ src/__tests__/test.test.ts
@@ -0,0 +1,5 @@
+import TestSample from '../TestSample';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(TestSample(1, 2)).toBe(3);
+});
